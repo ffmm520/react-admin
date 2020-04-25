7656f8d42102677d987c8bdfb5a20a047f8be060
mrhu <ffmm1220@163.com>
2020-04-25 11:31:58 +0800

    完成退出功能

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,25 +1,45 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
-import './index.less'
+import { withRouter } from 'react-router-dom'
+import { Button, Modal } from 'antd'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
+
 import { memoryStorage } from '../../utils/memoryUtils'
+import { removeStorage } from '../../utils/storageUtils'
+import './index.less'
+
+const { confirm } = Modal
 class Header extends Component {
-	state = {}
+	logout = () => {
+		confirm({
+			title: '确定退出吗?',
+      icon: <ExclamationCircleOutlined />,
+      okText: '确认',
+      cancelText: '取消',
+			onOk: () => {
+				removeStorage()
+				memoryStorage.user = {}
+				this.props.history.replace('/login')
+			}
+		})
+	}
 	render() {
 		return (
-			<div className="header">
-				<div className="header-top">
-					<span className="vendor-name">
+			<div className='header'>
+				<div className='header-top'>
+					<span className='vendor-name'>
 						hello, {memoryStorage.user.username}
 					</span>
-					<Button size="small">退出</Button>
+					<Button size='small' onClick={this.logout}>
+						退出
+					</Button>
 				</div>
-				<div className="header-bottom">
-					<div className="tab">首页</div>
-					<div className="weather">天气</div>
+				<div className='header-bottom'>
+					<div className='tab'>首页</div>
+					<div className='weather'>天气</div>
 				</div>
 			</div>
 		)
 	}
 }
 
-export default Header
+export default withRouter(Header)
